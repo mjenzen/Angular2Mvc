6c402db13384fbb2c976aaed6614f6ce7a0f64cc
jwall9108 <artworks1>
2017-07-13 10:03:12 -0400

    Code clean up

--- Angular2MVC/app/Components/state.component.js
+++ Angular2MVC/app/Components/state.component.js
@@ -11,7 +11,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var global_1 = require("../Shared/global");
-var dropDownValue_1 = require("../Model/dropDownValue");
 var observer_service_1 = require("../Services/observer.service");
 var state_service_1 = require("../Services/state.service");
 var state_1 = require("../Model/State/state");
@@ -20,29 +19,18 @@ var StateComponent = (function () {
         this._observerService = _observerService;
         this._stateService = _stateService;
         this.selectedState = new state_1.State();
-        this.selectedValue = new dropDownValue_1.DropdownValue(1, 'Virginia');
-        //need service to get all states here
-        this.values = [];
     }
     StateComponent.prototype.LoadStates = function () {
         var _this = this;
         this._stateService.get(global_1.Global.BASE_STATE_ENDPOINT)
             .subscribe(function (states) {
-            _this.bindStateDropDown(states);
-            _this.selectedStateFlagUri = "app/Assets/Images/" + states[0].Abbreviation + ".png";
             _this.selectedState = states[0];
             _this.states = states;
         }, function (error) { return _this.msg = error; });
     };
-    StateComponent.prototype.bindStateDropDown = function (States) {
-        for (var i = 0; i < States.length; i++) {
-            this.values.push(new dropDownValue_1.DropdownValue(States[i].Id, States[i].Name));
-        }
-    };
-    StateComponent.prototype.onChange = function (deviceValue) {
-        this.selectedState = this.states.filter(function (x) { return x.Id == deviceValue; })[0];
-        this.selectedStateFlagUri = "app/Assets/Images/" + this.states.filter(function (x) { return x.Id == deviceValue; })[0].Abbreviation + ".png";
-        this._observerService.changeState(deviceValue);
+    StateComponent.prototype.onChange = function (id) {
+        this.selectedState = this.states.filter(function (x) { return x.Id == id; })[0];
+        this._observerService.changeState(id);
     };
     StateComponent.prototype.ngOnInit = function () {
         this.LoadStates();
--- Angular2MVC/app/Components/state.component.ts
+++ Angular2MVC/app/Components/state.component.ts
@@ -17,41 +17,24 @@ import { State } from "../Model/State/state";
 
 export class StateComponent implements OnInit, OnDestroy {
   
-    item: number;
     msg: any;
     selectedState: State = new State();
     states: State[];
-    selectedStateFlagUri: string;
 
     constructor(private _observerService: ObserverService<number>, private _stateService: StateService) { }
- 
-    selectedValue: DropdownValue<number> = new DropdownValue(1, 'Virginia');
-
-    //need service to get all states here
-    values:Array<DropdownValue<number>> = [];
 
     LoadStates(): void {
         this._stateService.get(Global.BASE_STATE_ENDPOINT)
             .subscribe(states => {
-                this.bindStateDropDown(states);
-                this.selectedStateFlagUri = "app/Assets/Images/" + states[0].Abbreviation + ".png";
                 this.selectedState = states[0];
                 this.states = states;
             },
             error => this.msg = <any>error);
     }
 
-    bindStateDropDown(States: State[]) {
-        for (var i = 0; i < States.length; i++) {
-            this.values.push(new DropdownValue(States[i].Id, States[i].Name))
-       }
-    }
-
-    onChange(deviceValue: any) {
-
-        this.selectedState = this.states.filter(x => x.Id == deviceValue)[0]
-        this.selectedStateFlagUri = "app/Assets/Images/" + this.states.filter(x => x.Id == deviceValue)[0].Abbreviation + ".png";
-        this._observerService.changeState(deviceValue);
+    onChange(id: any) {
+        this.selectedState = this.states.filter(x => x.Id == id)[0];
+        this._observerService.changeState(id);
     }
 
     ngOnInit(): void {
