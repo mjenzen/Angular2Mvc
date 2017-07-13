ca4390926e4af7575a31865f3f0f01f1e4f241ba
jwall9108 <artworks1>
2017-07-13 09:34:34 -0400

    Adding api controllers

--- Angular2MVC/Controllers/VendorContactController - Copy.cs
+++ Angular2MVC/Controllers/VendorContactController - Copy.cs
@@ -1,44 +1,30 @@
 ﻿using Angular2MVC.Code;
 using Angular2MVC.DBContext;
-using Angular2MVC.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
 namespace Angular2MVC.Controllers
 {
-    public class StateController : BaseAPIController
+    public class LocationController : BaseAPIController
     {
         private StateRepository stateRepository;
 
-        public StateController()
+        public LocationController()
         {
             stateRepository = new StateRepository(new StateTrackerContext());
         }
 
         // GET api/<controller>
-        public HttpResponseMessage Get()
+        public void Get()
         {
-            var states = stateRepository.GetStates().Select(x =>
-                new StateModel()
-                {
-                    Id = x.StateId,
-                    Name = x.State1,
-                    Abbreviation = x.Abbreviation
-                }
-            ).ToList();
 
-            return ToJson(states);
+
         }
 
         // GET api/<controller>/5
-        public HttpResponseMessage Get(int id)
+        public void Get(int id)
         {
-            var state = new StateModel(stateRepository.GetStateById(id));
-            return ToJson(state);
+
         }
 
         // POST api/<controller>
---
+++ Angular2MVC/Models/StateModel.cs
@@ -0,0 +1,31 @@
+﻿using Angular2MVC.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular2MVC.Models
+{
+    public class StateModel
+    {
+        private State state;
+
+        public StateModel()
+        {
+        }
+
+        public StateModel(State state)
+        {
+            Id = state.StateId;
+            Name = state.State1;
+            Abbreviation = state.Abbreviation;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Abbreviation { get; set; }
+
+    }
+}
